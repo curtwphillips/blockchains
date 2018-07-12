26c80361039f53d79a706742bf96f4dc1ee62d4c
curtwphillips <curtwphillips@gmail.com>
2018-07-12 14:24:45 -0500

    update run.sh paths

--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
-#!/bin/bash
-# script assumes the blocks directory is in your home directory $HOME
+#!/usr/local/bin/bash
+# requires bash 4+
+
+# script assumes:
+# the blocks directory is in your home directory $HOME
+# you compiled bitcoind/litecoind in the ~/blocks/<coin>/server directory
+# otherwise you will need to update the paths below
 
 # command line arguments:
-# the default task is to start livenet nodes for btc, bch, and ltc
-# the coins to start must be specified or they will all start, with -btc, -bch, and/or -ltc
+# the default task with no passed in arguments is to start livenet nodes for btc, bch, and ltc
+# specify particular coin servers to start or stop with -btc, -bch, and/or -ltc
 # if you want to stop servers instead of starting them use -stop
 # if you want to operate on testnet servers instead of livenet use -testnet
 
@@ -19,28 +24,49 @@
 
 while test $# -gt 0
 do
-    case "$1" in
-        -stop) echo STOP=true
-            ;;
-        -testnet) TESTNET=true
-            ;;
-        -btc) BTC=true
-            ;;
-        -bch) BCH=true
-            ;;
-        -ltc) LTC=true
-            ;;
-        -*) echo "bad option $1"
-            exit 1 ;;
-        *) echo "bad option $1"
-            exit 1 ;;
-    esac
-    shift
+  case "$1" in
+    -stop) echo STOP=true
+      ;;
+    -testnet) TESTNET=true
+      ;;
+    -btc) BTC=true; COINS=("${COINS[@]}" 'btc')
+      ;;
+    -bch) BCH=true COINS=("${COINS[@]}" 'bch')
+      ;;
+    -ltc) LTC=true COINS=("${COINS[@]}" 'ltc')
+      ;;
+    -*) echo "bad option $1"
+      exit 1 ;;
+    *) echo "bad option $1"
+      exit 1 ;;
+  esac
+  shift
 done
 
+declare -A COINSERVERS
+COINSERVERS["btc"]='bitcoind'
+COINSERVERS["bch"]='bitcoind'
+COINSERVERS["ltc"]='litecoind'
+
 # if no coin args operate on all coins
-if [ ! "$BTC" ] && [ ! "$BCH" ] && [ ! "$LTC" ]; then
-    BTC=true
-    BCH=true
-    LTC=true
+if [ ! "$COINS" ]; then
+  COINS=('btc' 'bch' 'ltc')
 fi
+
+for COIN in "${COINS[@]}"
+do
+  if [ "$TESTNET"]; then
+    CONF="$HOME/blocks/$COIN/pruned/$COIN.pruned.testnet.conf"
+    GREPSTRING="blocks/$COIN/pruned/$COIN.pruned.testnet.conf"
+  else
+    CONF="$HOME/blocks/$COIN/pruned/$COIN.pruned.conf"
+    GREPSTRING="blocks/$COIN/pruned/$COIN.pruned.conf"
+  fi
+  if [ STOP = true ]; then
+    kill -SIGTERM $(ps aux | grep "$GREPSTRING" | grep -v grep | awk '{print $2}')
+    echo "stopping $COINSERVERS[$COIN] for $COIN using $GREPSTRING"
+  else
+    $HOME/blocks/$COIN/server/src/${COINSERVERS[${COIN}]} -daemon -datadir=$HOME/blocks/$COIN/pruned -conf=CONF
+    echo "starting ${COINSERVERS[${COIN}]} for $COIN using $HOME/blocks/$COIN/server/src/${COINSERVERS[${COIN}]}"
+  fi
+done
